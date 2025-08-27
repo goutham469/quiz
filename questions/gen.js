059c13a3348c3e7010eac3fe122c488db7778c27
goutham <uppinurigouthamreddy@gmail.com>
2025-08-27 22:54:02 +0530

    api key ecpose fixed

--- questions/gen.js
+++ questions/gen.js
@@ -1,7 +1,7 @@
 const { GoogleGenAI } = require("@google/genai");
 const fs = require("fs");
 
-const GOOGLE_GEN_AI_API_KEY = "AIzaSyAkJNyzl0m2fcr2XZu4ch_RxYw3lnFaPfA";
+const GOOGLE_GEN_AI_API_KEY = "";
 const ai = new GoogleGenAI({ apiKey: GOOGLE_GEN_AI_API_KEY });
 
 /**
